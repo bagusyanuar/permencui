8162bd7880b9e9957031e057e4a42daea529ceb6
bagusyanuar <bagus.yanuar613@gmail.com>
2023-06-07 14:07:24 +0700

    design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,22 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+
         .footer {
             position: fixed;
             bottom: 0;
@@ -37,17 +47,21 @@
 
 
         .w-280 {
-            width: 350px !important;
+            /*width: 350px !important;*/
+            left: 0 !important;
         }
 
         .sidebar-display {
             display: block !important;
         }
+
         .sidebar {
-            display: none;
+            /*display: none;*/
+            width: 350px !important;
             height: 100vh;
-            width: 0;
-            position: fixed;
+            /*width: 0;*/
+            position: absolute;
+            left: 0;
             background-color: #fe9ff5;
             z-index: 9999;
         }
@@ -59,15 +73,36 @@
             -o-transition: all 1s;
         }
 
+        .sidebar-menu {
+            list-style-type: none;
+        }
+        .sidebar-item {
+            height: 50px;
+            transition: all ease-in-out .5s;
+        }
 
+        .sidebar-item:hover {
+            background-color: blueviolet;
+        }
     </style>
 </head>
 <body class="position-relative">
-    <div class="sidebar flex-column align-items-center" id="sidebar">
-        <div>
-            <img src="{{ asset('/assets/logo-permencui.png') }}" height="80" alt="logo-permencui">
-        </div>
+<div class="sidebar d-flex flex-column justify-content-center position-absolute" id="sidebar">
+    <div class="w-100 text-center">
+        <img src="{{ asset('/assets/logo-permencui.png') }}" height="150" alt="logo-permencui">
     </div>
+    <div>
+        <ul class="sidebar-menu">
+            <li class="sidebar-item d-flex align-items-center">
+                <a href="#" class="">
+                    Tentang Permencui
+                </a>
+            </li>
+            <li>Menu Permencui</li>
+            <li>Simulasi Penjualan</li>
+        </ul>
+    </div>
+</div>
 
 
 <div class="" style="">
@@ -144,14 +179,10 @@
             e.preventDefault();
             e.stopPropagation();
             $('#sidebar').toggleClass('w-280')
-            $('#sidebar').toggleClass('d-block')
         })
         $(document).on('click', function () {
             if ($('#sidebar').hasClass('w-280')) {
                 $('#sidebar').toggleClass('w-280');
-                // $('#sidebar-parent').toggleClass('d-none');
-                $('#sidebar').toggleClass('d-block')
-                console.log('has class')
             }
         })
     })
