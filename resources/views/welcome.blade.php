f2c62b2b68e9048cc80c83fd1494b63e16a5fc24
bagusyanuar <bagus.yanuar613@gmail.com>
2023-06-20 20:51:32 +0700

    carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,10 @@
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <style>
+        html {
+            scroll-behavior: smooth;
+        }
+
         body {
             font-family: 'Poppins', sans-serif;
         }
@@ -181,6 +185,41 @@
             <div id="page_4">
                 <img src="{{ asset('/assets/page_4_landing_page.jpg') }}" alt="image 3" class="w-100">
             </div>
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+{{--                <div class="carousel-indicators">--}}
+{{--                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>--}}
+{{--                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>--}}
+{{--                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>--}}
+{{--                </div>--}}
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="{{ asset('/assets/slider1.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('/assets/slider2.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('/assets/slider3.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('/assets/slider4.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('/assets/slider5.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('/assets/slider6.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
             <div id="page_5">
                 <img src="{{ asset('/assets/page_5_landing_page.jpg') }}" alt="image 3" class="w-100">
             </div>
@@ -202,19 +241,7 @@
             <div id="page_11">
                 <img src="{{ asset('/assets/page_11_landing_page.jpg') }}" alt="image 3" class="w-100">
             </div>
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="{{ asset('/assets/slide_show_1.jpeg') }}" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ asset('/assets/slide_show_2.JPG') }}" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ asset('/assets/slide_show_3.JPG') }}" class="d-block w-100" alt="...">
-                    </div>
-                </div>
-            </div>
+
             <div class="footer pt-3 pb-3">
                 <div class="me-3">
                     <a href="#" class="btn btn-success d-flex align-items-center btn-whatsapp" id="btn-whatsapp" style="font-size: 14px">
